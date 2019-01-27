ee69dc3c8c1e3912cd2ee23b6b661dc1d16f3d0b
Jayesh Badwaik <public@jayeshbadwaik.in>
2019-01-27 20:03:08 +0100

    + update

--- zell-llvm70-toolchain/toolchain.cmake
+++ zell-llvm70-toolchain/toolchain.cmake
@@ -1,8 +1,9 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../../../compiler/llvm-7.0/etc/compiler.cmake)
 set(INSTALL_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../")
-list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${INSTALL_PREFIX})
+set(CMAKE_SYSTEM_PREFIX_PATH ${INSTALL_PREFIX})
 include_directories(SYSTEM "${INSTALL_PREFIX}/include")
 link_directories(AFTER "${INSTALL_PREFIX}/lib")
 
 # Library Specific Variables
 set(BOOST_ROOT ${INSTALL_PREFIX})
+set(nlohmann_json_ROOT ${INSTALL_PREFIX})
