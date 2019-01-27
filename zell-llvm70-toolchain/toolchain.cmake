2b432745137061df4b313892d0cfa1e5be2c4314
Jayesh Badwaik <public@jayeshbadwaik.in>
2019-01-27 16:41:07 +0100

    + added json package

--- zell-llvm70-toolchain/toolchain.cmake
+++ zell-llvm70-toolchain/toolchain.cmake
@@ -3,3 +3,6 @@ set(INSTALL_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../")
 list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${INSTALL_PREFIX})
 include_directories(SYSTEM "${INSTALL_PREFIX}/include")
 link_directories(AFTER "${INSTALL_PREFIX}/lib")
+
+# Library Specific Variables
+set(BOOST_ROOT ${INSTALL_PREFIX})
