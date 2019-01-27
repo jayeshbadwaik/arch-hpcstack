1f2915a1e38fe596c37681758050181920454030
Jayesh Badwaik <public@jayeshbadwaik.in>
2019-01-27 20:42:18 +0100

    + update

--- generic-llvm70-toolchain/toolchain.cmake
+++ generic-llvm70-toolchain/toolchain.cmake
@@ -1,9 +1,9 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../../../compiler/llvm-7.0/etc/compiler.cmake)
 set(INSTALL_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../")
-set(CMAKE_SYSTEM_PREFIX_PATH ${INSTALL_PREFIX})
+list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${INSTALL_PREFIX})
 include_directories(SYSTEM "${INSTALL_PREFIX}/include")
 link_directories(AFTER "${INSTALL_PREFIX}/lib")
 
 # Library Specific Variables
-set(BOOST_ROOT ${INSTALL_PREFIX})
+set(Boost_ROOT ${INSTALL_PREFIX})
 set(nlohmann_json_ROOT ${INSTALL_PREFIX})
